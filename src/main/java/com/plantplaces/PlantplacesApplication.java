498318527f7c3e14bed7c16030741770553348f8
Administrator <Administrator@DESKTOP-099UQRG>
2021-09-07 14:37:36 -0400

    update .gitignore

--- src/main/java/com/plantplaces/PlantplacesApplication.java
+++ src/main/java/com/plantplaces/PlantplacesApplication.java
@@ -1,4 +1,4 @@
-package com.plantplaces.plantplaces;
+package com.plantplaces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
